88d4598342d5a17d89ec3de4db3755d0618ad08b
unknown <paddymchale@hotmail.com>
2018-02-25 15:23:08 +0000

    functions, constructors, arrays, slices

--- temp.go
+++ temp.go
@@ -5,20 +5,34 @@ import (
 )
 
 func main() {
-	//power := getPower()
+	player := &Player{Name: "a"}
+	player.Power = 9000
 
-	//name, power := "Goku", 9000
-	//fmt.Printf("%s's power is over %d\n", name, power)
+	//Improve(player);
+	player.Improve()
 
-	a, exists := power("pat")
-	//fmt.Printf(a)
-	//fmt.Printf(exists)
-	fmt.Printf("over %d\n",a)
-	if(exists == false){
+	fmt.Println(player.Power)
 
-	}
+	player2 := NewPlayer("s",100)
+	fmt.Println(player2.Power)
 
+	//scores := [4]int{9001, 9333, 212, 33}
+	/*for index, value := range scores {
+		fmt.Println(index)
+		fmt.Println(value)
+	}*/
+
+	//scores := []int{1,4,293,4,9}
+ 
+
+	//scores := make([]int, 10)
+	scores := make([]int, 0, 10)
+	scores = scores[0:8]
+	scores[7] = 9033
+	fmt.Println(scores)
 }
+
+
 
 func getPower() int {
 	return 9001
@@ -33,7 +47,28 @@ func add(a int, b int) int {
 func power(name string) (int, bool) {
 	return 1,true
 }
+
 type Player struct {
 	Name string
 	Power int
+	Father *Player
 }
+
+func (p *Player) Improve (){
+	p.Power += 10000
+}
+
+func NewPlayer(name string, power int) *Player {
+	return &Player{
+		Name: name,
+		Power: power,
+	}
+}
+
+/*func NewPlayer(name string, power int) Player {
+	return Player{
+		Name: name,
+		Power: power,
+	}
+}*/
+
