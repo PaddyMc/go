2ea9427e0a722415a94c27c445768d827db781ee
unknown <paddymchale@hotmail.com>
2018-03-04 14:29:55 +0000

    interfaces

--- shopping/main/main.go
+++ shopping/main/main.go
@@ -3,9 +3,31 @@ package main
 import (
 	"shopping"
 	"fmt"
-	"github.com/mattn/go-sqlite3"
+	//"github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	fmt.Println(shopping.PriceCheck(4343))
 }
+
+/*type Logger interface {
+	Log(message string)
+}
+
+type SqlLogger struct {}
+type ConsoleLogger struct {}
+type FileLogger struct {}
+
+type Server struct {
+	logger Logger
+}
+
+func process(logger Logger) {
+	logger.Log("hello!")
+}
+
+type ConsoleLogger struct {}
+	func (l ConsoleLogger) Log(message string) {
+			fmt.Println(message)
+}
+*/
